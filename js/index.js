30f03af60ad2a5f33628f24c04112297403b0b11
Rodrigo Enrique Ahumada Espinoza <rod.ahumada@duocuc.cl>
2023-09-15 14:33:28 -0300

    ejercicio terminado

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
-alert("estas en index.js")
+function likeBtn(){
+  alert(`This page says
+Ninja was liked`);
+}
+
+function removerBoton(element){
+  element.closest('#add').remove();
+}
+
+function logBtn(element){
+  let btnLog = document.querySelector('#log')
+  let textBtnLog = btnLog.innerText;
+  if(textBtnLog === "Login"){
+    btnLog.textContent = "Logout";
+  }else{
+    btnLog.textContent = "Login";
+  }  
+}
